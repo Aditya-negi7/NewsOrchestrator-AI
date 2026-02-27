10fea5b795e53ed5056b98875a1bf20dde9bb90c
Aditya Negi <104727097+Aditya-negi7@users.noreply.github.com>
2026-02-27 15:55:03 +0530

    Update crew_setup.py

--- crew_setup.py
+++ crew_setup.py
@@ -1,15 +1,11 @@
 from crewai import Crew
-from agents import create_agents
-from tasks import create_tasks
+from agents import smart_researcher, smart_writer
+from tasks import research_task, write_task
 
 def create_crew():
-    news_researcher, news_writer = create_agents()
-    tasks = create_tasks(news_researcher, news_writer)
-
     crew = Crew(
-        agents=[news_researcher, news_writer],
-        tasks=tasks,
-        verbose=True
+        agents=[smart_researcher, smart_writer],
+        tasks=[research_task, write_task],
+        verbose=2
     )
-
     return crew
