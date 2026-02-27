4850d1d0cef3f8da4ba87ed71bcebf26ca92c1d7
Aditya Negi <104727097+Aditya-negi7@users.noreply.github.com>
2026-02-27 16:00:59 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -7,24 +7,23 @@ def create_agents():
 
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
-        temperature=1,
+        temperature=0.7,
         google_api_key=os.getenv("GOOGLE_API_KEY")
     )
 
     news_researcher = Agent(
-        role="News Researcher",
-        goal="Research latest news",
-        backstory="Expert news analyst",
+        role="Senior Real-Time Research Analyst",
+        goal="Research the given topic using real-time internet data.",
+        backstory="Expert analyst who always uses tools and never hallucinates.",
         tools=[tool],
         verbose=True,
         llm=llm
     )
 
     news_writer = Agent(
-        role="News Writer",
-        goal="Write structured article",
-        backstory="Professional journalist",
-        tools=[tool],
+        role="Professional News Writer",
+        goal="Write structured and factual articles based only on provided research.",
+        backstory="Professional journalist who never invents information.",
         verbose=True,
         llm=llm
     )
